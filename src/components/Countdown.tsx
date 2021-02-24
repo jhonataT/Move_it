2e6ff616de7b1aee77fe589912062ea528374fc7
Jhn <jhonata.tenorio2526@gmail.com>
2021-02-24 18:53:28 -0300

    countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,8 +2,16 @@ import styles from '../styles/components/Countdown.module.css';
 
 export const Countdown = () => {
     return (
-        <div>
-
+        <div className={styles.countdownContainer}>
+            <div>
+                <span>2</span>
+                <span>5</span>
+            </div>
+            <span>:</span>
+            <div>
+                <span>0</span>
+                <span>0</span>
+            </div>
         </div>
     );
 }
